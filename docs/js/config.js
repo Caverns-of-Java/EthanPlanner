0c63903de3dee810559a16f563b56c469faf030d
Ken <youremail@example.com>
2026-04-05 13:44:13 +1000

    added docs things

---
+++ docs/js/config.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = "https://script.google.com/macros/s/AKfycbyhSTtNEPFCqcTvKzOjaZ5zBLocSMwjGTaz4EQsJIxj3k_sKKgt7Da_eBXAkMzPYf6Slw/exec";
+export const DEFAULT_COLOUR = "#e75480";
